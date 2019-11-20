5b889f745126af293c5f68bb6191362523fa2013
Guido Rombolá <guidorombola@gmail.com>
2019-11-20 16:07:13 -0300

    Libera PWM despues de hacer sonar buzzer

--- src/modelos/ControladorDeAlertas.cpp
+++ src/modelos/ControladorDeAlertas.cpp
@@ -2,11 +2,12 @@
 
 ControladorDeAlertas::ControladorDeAlertas(){
     ledcSetup(CANAL_PWM, FRECUENCIA_BUZZER_OFF, RESOLUCION_PWM);
-    ledcAttachPin(PIN_BUZZER, FRECUENCIA_BUZZER_OFF);
 }
 
 void ControladorDeAlertas::comunicarNuevaNotificacion(){
+    ledcAttachPin(PIN_BUZZER, FRECUENCIA_BUZZER_OFF);
     ledcWriteTone(0, FRECUENCIA_BUZZER_ON);
     delay(100);
     ledcWriteTone(0, FRECUENCIA_BUZZER_OFF);
+    ledcDetachPin(PIN_BUZZER);
 }
